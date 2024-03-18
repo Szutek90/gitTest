562b57947225409017e37381547862355166250c
PAWEL <szutkopawel@gmail.com>
2024-03-18 11:34:05 +0100

    added counting

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        final int X = 5;
+        for (int i = 0; i < X; i++) {
+            hello();
+        }
     }
 
     public static void hello(){
